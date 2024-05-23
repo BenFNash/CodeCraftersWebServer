3e578ef66be9d08d3bfdb8137f9f61b75ad162cd
Ben Nash <benfnash@gmail.com>
2024-05-23 22:37:55 +0100

    renamed l to ln

--- app/server.go
+++ app/server.go
@@ -27,12 +27,9 @@ func handler(conn net.Conn) {
 
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	ln, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
@@ -41,7 +38,7 @@ func main() {
   fmt.Println("Listening on port 4221")
   defer ln.Close()
 
-  conn, err := l.Accept()
+  conn, err := ln.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
